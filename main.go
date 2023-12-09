2a124141b5e08b972358af0aa8e3ee54b4436eb2
surreal <2002vaibhavlakhera3@gmail.com>
2023-12-09 18:12:39 +0530

    feat: create scan.go and move scanner code to it

--- main.go
+++ main.go
@@ -2,10 +2,108 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"io"
+	"strings"
+	"bufio"
 )
 
 func scan(path string) {
 	print("scan")
+	fmt.Printf("Found folders: \n \n")
+	repositories := recursiveScanFolder(folder)
+	filePath := getDotFilePath()
+	addNewSliceElementsToFile(filePath, repositories)
+	fmt.Printf("Successfully added \n")
+}
+
+func scanGitFolders(folders []string, folder string) []string {
+	folder = string.TrimSuffix(folder, "/")
+
+	f, err := os.Open(folder)
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files, err := f.Readdir(-1)
+	f.close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var path string
+
+	for _, file := range files {
+		if file.IsDir() {
+			path = path + "/" + file.Name()
+			if file.Name() == ".git" {
+				path = strings.TrimSuffix(path, "/.git")
+				fmt.Println(path)
+				folders = append(folders, path)
+				continue			
+			}
+
+			if file.Name() == "vendor" || file.Name() == "node_modules" {
+				continue
+			}
+
+			folders = scanGitFolders(folders, path)
+		}
+
+	}
+
+	return folders 
+}
+
+func recursiveScanFolder(folder string) []string {
+	return scanGitFolders(make([]string, 0) folder)
+}
+
+func getDotFilePath() string {
+	usr, err := user.Current()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dotFile := usr.HomeDir + "/.gogitlocalstats"
+
+	return dotFile
+}
+
+type User struct {
+	Uid string 
+	Gid string 
+	Username string
+	Name string
+	HomeDir string
+}
+
+func addNewSliceElementsToFile(filePath string, newRepos []string) {
+	existingRepos := parseFileLinesToSlice(filePath)
+	repos := joinSlices(newRepos, existingRepos)
+	dumpStringSlicesToFile(repos, filePath)
+}
+
+func parseFileLinesToSlice(filePath string) []string {
+	f := openFile(filePath)
+	defer f.Close()
+
+	var lines []string
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		if err != io.EOF {
+			panic(err)
+		}
+	}
 }
 
 func stats(email string) {
---
+++ scan.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io"
+	"io/ioutil"
+	"log"
+	"os/user"
+	"strings"
+	"bufio"
+)
+
+func getDotFilePath() string {
+	usr, err := user.Current()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dotFile := usr.HomeDir + "/.gogitlocalstats"
+
+	return dotFile
+}
+
+func openFile(filePath string) *os.File {
+	f, err := os.OpenFile(filePath string, os.O_Append|os.O_WRONLY, 0755)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			_, err := os.Create(filePath)
+
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		else {
+			panic(err)
+		}
+	}
+
+	return f
+}
+
+func parseFileLinesToSlice(filePath string) []string {
+	f := openFile(filePath)
+	defer f.Close()
+
+	var lines []string
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		if err != io.EOF {
+			panic(err)
+		}
+	}
+
+	return lines
+}
+
+func sliceContains(slice []string, value string) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+
+	return false
+}
+
+func joinSlices(new []string, existing []string) []string {
+	for _, i := range new {
+		if !sliceContains(existing, i) {
+			existing = append(existing, i)
+		}
+	}
+
+	return existing
+}
+
+func dumpStringSlicesToFile(repos []string, filePath string) {
+	content := strings.Join(repos, "\n")
+	ioutil.WriteFile(filePath, []byte(content), 0755)
+}
+
+func addNewSliceElementsToFile(filePath string, newRepos []string) {
+	existingRepos := parseFileLinesToSlice(filePath)
+	repos := joinSlices(newRepos, existingRepos)
+	dumpStringSlicesToFile(repos, filePath)
+}
+
+func scan(path string) {
+	print("scan")
+	fmt.Printf("Found folders: \n \n")
+	repositories := recursiveScanFolder(folder)
+	filePath := getDotFilePath()
+	addNewSliceElementsToFile(filePath, repositories)
+	fmt.Printf("Successfully added \n")
+}
+
+func scanGitFolders(folders []string, folder string) []string {
+	folder = string.TrimSuffix(folder, "/")
+
+	f, err := os.Open(folder)
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files, err := f.Readdir(-1)
+	f.close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var path string
+
+	for _, file := range files {
+		if file.IsDir() {
+			path = path + "/" + file.Name()
+			if file.Name() == ".git" {
+				path = strings.TrimSuffix(path, "/.git")
+				fmt.Println(path)
+				folders = append(folders, path)
+				continue			
+			}
+
+			if file.Name() == "vendor" || file.Name() == "node_modules" {
+				continue
+			}
+
+			folders = scanGitFolders(folders, path)
+		}
+
+	}
+
+	return folders 
+}
+
+func recursiveScanFolder(folder string) []string {
+	return scanGitFolders(make([]string, 0) folder)
+}
